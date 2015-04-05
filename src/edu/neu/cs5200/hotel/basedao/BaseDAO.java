a3fbd60ab39064781f8bde2da54e0b88955cfac1
jueyang1031 <jueyang@ccs.neu.edu>
2015-04-05 17:33:08 -0400

    basedao

---
+++ src/edu/neu/cs5200/hotel/basedao/BaseDAO.java
@@ -0,0 +1,64 @@
+package edu.neu.cs5200.hotel.basedao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class BaseDAO<T> {
+
+	public EntityManagerFactory factory = Persistence.createEntityManagerFactory("HotelReservation");
+	public EntityManager em = factory.createEntityManager();
+	
+	public T create(T object) {
+		em.getTransaction().begin();;
+		em.persist(object);
+		em.flush();
+		em.getTransaction().commit();
+		return object;
+	}
+	
+	public T update(T object) {
+		em.getTransaction().begin();
+		em.merge(object);
+		em.getTransaction().commit();
+		return object;
+	}
+	
+	public Boolean delete(T object) {
+		if (em.contains(object)) {
+			em.remove(object);
+			return true;
+		}else return false;
+	}
+	
+	public T getById(Class<T> clz, Integer id) {
+		return em.find(clz, id);
+	}
+	
+	public List<T> query(Class<T> clz, String scope, List paras) {
+		Query query = em.createQuery("FROM" + clz + "WHERE" + scope);
+		int parameterIndex = 0;
+        if (paras != null && paras.size() > 0) {  
+            for (Object obj : paras) {  
+                query.setParameter(parameterIndex++, obj);  
+            }  
+        }
+        return query.getResultList();
+	}
+	
+	public List<T> executeNativeQuery(String nameNativeQuery, List parasName, List paras) {
+		Query query = em.createNativeQuery(nameNativeQuery);
+		if (paras != null && paras.size() > 0) {  
+        	int i = 0;
+            for (Object obj : paras) {
+            	query.setParameter((String)parasName.get(i), obj);  
+            	++i;
+            }  
+        }  
+        
+		return query.getResultList();
+	}
+}
--- src/edu/neu/cs5200/hotel/dao/CustomerDAO.java
+++ src/edu/neu/cs5200/hotel/dao/CustomerDAO.java
@@ -1,5 +1,17 @@
 package edu.neu.cs5200.hotel.dao;
 
+import edu.neu.cs5200.hotel.basedao.BaseDAO;
+import edu.neu.cs5200.hotel.domain.Customer;
+
 public class CustomerDAO {
 
+	private BaseDAO<Customer> baseDAO;
+
+	public CustomerDAO() {
+		super();
+		this.baseDAO = new BaseDAO<Customer>();
+	}
+	
+	
+	
 }
