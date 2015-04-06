3a44c9934e12c68f90d3ce924dbc5442a51bb43b
shengjiang1989 <jiang.she@husky.neu.edu>
2015-04-05 22:10:12 -0400

    jj

---
+++ src/edu/neu/cs5200/hotel/test/CreateAdminTest.java
@@ -0,0 +1,16 @@
+package edu.neu.cs5200.hotel.test;
+
+import edu.neu.cs5200.hotel.request.CreateAdminReq;
+import edu.neu.cs5200.hotel.service.RegisterAdmin;
+
+public class CreateAdminTest {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RegisterAdmin registerAdmin = new RegisterAdmin();
+		CreateAdminReq createAdminReq = new CreateAdminReq();
+		createAdminReq.setPassword("lol");
+		createAdminReq.setSponsoraccount("bigguy");
+		createAdminReq.setUsername("bigguy");
+		registerAdmin.createAdmin(createAdminReq);
+		}
+}
