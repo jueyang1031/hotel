49c46b2a1825bccf5d3b486f0be6012d238c3853
shengjiang1989 <jiang.she@husky.neu.edu>
2015-04-05 21:36:06 -0400

    re

--- src/edu/neu/cs5200/hotel/dao/AdminDAO.java
+++ src/edu/neu/cs5200/hotel/dao/AdminDAO.java
@@ -1,5 +1,21 @@
 package edu.neu.cs5200.hotel.dao;
 
+import edu.neu.cs5200.hotel.basedao.BaseDAO;
+import edu.neu.cs5200.hotel.domain.Admin;
+
 public class AdminDAO {
+
+		private BaseDAO<Admin> baseDAO;
+
+		public AdminDAO() {
+			super();
+			this.baseDAO = new BaseDAO<Admin>();
+		}
+		
+public void createAdmin(Admin admin) {
+			// TODO Auto-generated method stub
+		baseDAO.create(admin);
+		
+	}
 
 }
--- src/edu/neu/cs5200/hotel/service/RegisterAdmin.java
+++ src/edu/neu/cs5200/hotel/service/RegisterAdmin.java
@@ -1,5 +1,18 @@
 package edu.neu.cs5200.hotel.service;
 
-public class RegisterAdmin {
+import edu.neu.cs5200.hotel.dao.AdminDAO;
+import edu.neu.cs5200.hotel.dao.CustomerDAO;
+import edu.neu.cs5200.hotel.domain.Admin;
+import edu.neu.cs5200.hotel.request.CreateAdminReq;
 
+public class RegisterAdmin {
+	public void createAdmin(CreateAdminReq createAdminReq){
+		Admin admin = new Admin();
+		admin.setPassword(createAdminReq.getPassword());
+		admin.setSponsoraccount(createAdminReq.getSponsoraccount());
+		admin.setUsername(createAdminReq.getUsername());
+		
+		AdminDAO adminDAO = new AdminDAO();
+		adminDAO.createAdmin(admin);
+	}
 }
