f7e3c69318c48108712b5939b51d83c5795da20e
alirujinga <lirujing0905@hotmail.com>
2015-04-05 22:58:47 -0400

    auto to indentity

--- src/edu/neu/cs5200/hotel/domain/Orderdate.java
+++ src/edu/neu/cs5200/hotel/domain/Orderdate.java
@@ -15,7 +15,7 @@ public class Orderdate implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 
 	@Temporal(TemporalType.DATE)
